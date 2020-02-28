84456c93515698b69a1b71e6f4492e1b5306c19c
Romain Tello <romain.tello@gmail.com>
2020-02-28 10:02:58 +0100

    Refactor DOM change for answers

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ $('#question-id').html('1');
 // Modifie le texte de la question
 $('#current-question-text').html(questionData[0].text);
 // Modifie le texte des 4 réponses possibles à la question
-$('#answer1-caption').html(questionData[0].answer1);
-$('#answer2-caption').html(questionData[0].answer2);
-$('#answer3-caption').html(questionData[0].answer3);
-$('#answer4-caption').html(questionData[0].answer4);
+for (var i = 1; i <= 4; i += 1) {
+  $('#answer' + i + '-caption').html(questionData[0]['answer' + i]);
+}
